fbe093b79a95f32785be5173b1515310c5f9ac1b
Junin <juninpompeia.fi@gmail.com>
2023-08-24 11:35:07 -0300

    24.08

---
+++ gitconfig.bat
@@ -0,0 +1,2 @@
+git config --global user.name "Junin"
+git config --global user.email "juninpompeia.fi@gmail.com"
--- index.php
+++ index.php
@@ -1,21 +1,5 @@
 <?php
+require_once __DIR__ . '/Router.php';
+$request = $_SERVER['REQUEST_URI'];
+(new Router())->run($request);
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-
-    <style>
-        .topborder {margin-left: 10px;}
-    </style>
-</head>
-<body>
-    <div class="topborder">
-        <h1> HomePage </h1>
-    </div>
-</body>
-</html>
